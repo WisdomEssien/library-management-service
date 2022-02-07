184dc5dc3b2ef32fbc23ed57d514e64ca5b16195
wisdom.essien <wisdom.essien@etranzactng.com>
2022-02-07 06:54:38 +0100

    project setup and minimum configuration

---
+++ src/test/java/com/wis/libraryservice/LibraryserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wis.libraryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
